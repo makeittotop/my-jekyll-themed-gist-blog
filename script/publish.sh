55f94dae0937228ed2069e9d6a6bfe55c56d0448
Abhishek Pareek <root@abhishekpareek.io>
2016-06-15 17:44:51 +0400

    Add cloudfront invalidation to the mix

--- script/publish.sh
+++ script/publish.sh
@@ -4,3 +4,5 @@ PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=abhishekpareek.io
 DIR=_site/
 aws s3 sync $DIR s3://$BUCKET/ 
+aws configure set preview.cloudfront true
+aws cloudfront create-invalidation --distribution-id  $DISTRIBUTION_ID --paths "*"
